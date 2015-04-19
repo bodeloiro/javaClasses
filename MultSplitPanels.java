3f309ccb40e1c327909ab643ae3625fde6ac33b8
Robinson Donizete de Carvalho <bodeloiro@bol.com.br>
2015-04-18 21:25:24 -0300

    Update MultSplitPanels.java

--- MultSplitPanels.java
+++ MultSplitPanels.java
@@ -14,7 +14,7 @@ import javax.swing.JSplitPane;
  *
  */
 public class JSplitPaneMultiTabs extends JSplitPane {
-	private ArrayList<JSplitPane> ecanpsulationList = new ArrayList<JSplitPane>();
+	private ArrayList<JSplitPane> encapsulationList = new ArrayList<JSplitPane>();
 	private int numberOfComponents = 1;
 	private int sizeOfDivision = 6;
 
@@ -25,24 +25,25 @@ public class JSplitPaneMultiTabs extends JSplitPane {
 		super();
 		this.setLeftComponent(null);
 		this.setBorder(null);
-		ecanpsulationList.add(this);
+		encapsulationList.add(this);
 		setAllBorders(sizeOfDivision);
 	}
 
 	/**
 	 * 
-	 * @param comp - adds a Component to the Pane
+	 * @param comp
+	 *            - adds a Component to the Pane
 	 */
 	public void addComponent(Component comp) {
 		JSplitPane capsule = new JSplitPane();
-
 		capsule.setRightComponent(null);
 		capsule.setLeftComponent(comp);
 		capsule.setDividerSize(sizeOfDivision);
 		capsule.setBorder(null);
 
-		ecanpsulationList.get(numberOfComponents - 1).setRightComponent(capsule);
-		ecanpsulationList.add(capsule);
+		encapsulationList.get(numberOfComponents - 1)
+				.setRightComponent(capsule);
+		encapsulationList.add(capsule);
 		numberOfComponents++;
 		this.fixWeights();
 	}
@@ -58,19 +59,20 @@ public class JSplitPaneMultiTabs extends JSplitPane {
 	 */
 	public void setAlignment(int orientation) {
 		for (int i = 0; i < numberOfComponents; i++) {
-			ecanpsulationList.get(i).setOrientation(orientation);
+			encapsulationList.get(i).setOrientation(orientation);
 
 		}
 	}
 
 	/**
 	 * 
-	 * @param newSize - resizes the borders of the all the Components of the Screen
+	 * @param newSize
+	 *            - resizes the borders of the all the Components of the Screen
 	 */
 	public void setAllBorders(int newSize) {
 		this.setDividerSize(newSize);
 		for (int i = 0; i < numberOfComponents; i++) {
-			ecanpsulationList.get(i).setDividerSize(newSize);
+			encapsulationList.get(i).setDividerSize(newSize);
 		}
 
 	}
@@ -79,13 +81,106 @@ public class JSplitPaneMultiTabs extends JSplitPane {
 	 * each Component added needs to be readapteded to the screen
 	 */
 	private void fixWeights() {
-		ecanpsulationList.get(0).setResizeWeight(1.0);
+		encapsulationList.get(0).setResizeWeight(1.0);
 		for (int i = 1; i < numberOfComponents; i++) {
 			double resize = (double) 1 / (double) (i + 1);
-			ecanpsulationList.get(numberOfComponents - i - 1).setResizeWeight(
-					resize);
+			encapsulationList.get(numberOfComponents - i - 1).setResizeWeight(resize);
 		}
-		ecanpsulationList.get(numberOfComponents - 1).setResizeWeight(0.0);
+		encapsulationList.get(numberOfComponents - 1).setResizeWeight(0.0);
 	}
 
+	/**
+	 * 
+	 * @param comp
+	 *            - Component to be removed
+	 */
+	public void removeComponent(Component comp) {
+		for (int i = 0; i < numberOfComponents; i++) {
+			//treats when there are just 2 elements
+			if (numberOfComponents == 2) {
+				if (comp == encapsulationList.get(i).getLeftComponent()) {
+					System.out.println("Valor de ==2 : " + i);
+					encapsulationList.remove(i);
+					numberOfComponents--;
+					encapsulationList.get(0).setRightComponent(null);
+				}
+
+			}else// when there are more than 2 elements but not the final element
+			if (numberOfComponents > 2 && i != (numberOfComponents - 1)) {
+				if (comp == encapsulationList.get(i).getLeftComponent()) {
+					System.out.println("Valor de >2 : " + i);
+					encapsulationList.get(i - 1).setRightComponent(encapsulationList.get(i + 1));
+					encapsulationList.remove(i);
+					numberOfComponents--;
+
+				}
+
+			}else// treats the final element
+			if (i == (numberOfComponents - 1)) {
+				if (comp == encapsulationList.get(i).getLeftComponent()) {
+					System.out.println("Valor de i== -1 : " + i);
+					encapsulationList.get(i - 1).setRightComponent(null);
+					encapsulationList.remove(i);
+					numberOfComponents--;
+
+				}
+			}
+			
+			this.fixWeights();
+
+		}
+
+	}
+	/**
+	 * 
+	 * @param index - index to remove the items
+	 */
+	public void removeComponent(int index) {
+		index = index + 1 ;
+		for (int i = 1; i < numberOfComponents; i++) {
+			//treats when there are just 2 elements
+			if (numberOfComponents == 2) {
+				if (index == i) {
+					System.out.println("Valor de ==2 : " + i);
+					encapsulationList.remove(i);
+					numberOfComponents--;
+					encapsulationList.get(0).setRightComponent(null);
+				}
+
+			}else// when there are more than 2 elements but not the final element
+			if (numberOfComponents > 2 && i != (numberOfComponents - 1)) {
+				if (index == i) {
+					System.out.println("Valor de >2 : " + i);
+					encapsulationList.get(i - 1).setRightComponent(encapsulationList.get(i + 1));
+					encapsulationList.remove(i);
+					numberOfComponents--;
+
+				}
+
+			}else// treats the final element
+			if (i == (numberOfComponents - 1)) {
+				if (index == i) {
+					System.out.println("Valor de i== -1 : " + i);
+					encapsulationList.get(i - 1).setRightComponent(null);
+					encapsulationList.remove(i);
+					numberOfComponents--;
+
+				}
+			}
+			
+			this.fixWeights();
+
+		}
+
+	}
+
+	public int getNumberOfComponents() {
+		return numberOfComponents;
+	}
+
+	
+	
+	
+	
+	
 }
